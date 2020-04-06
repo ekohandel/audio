f050c53184b9a83f70da4cf52a89b36b1d64b5cc
Abe Kohandel <abe@electronshepherds.com>
2020-04-05 18:03:53 -0700

    Pin to Zephyr v2.2.0

--- fw/app/scripts/build.sh
+++ fw/app/scripts/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 west update
-west zephyr-export
 pip3 install --user -r ./zephyr/scripts/requirements.txt
 source ./zephyr/zephyr-env.sh
-west build -p auto -b $1 ./app -- -DBOARD_ROOT=$(pwd)/app
+west build -b $1 ./app -- -DBOARD_ROOT=$(pwd)/app
